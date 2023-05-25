5ae0261ed070ead007383963589841ef903c6aaa
Kyrre Gjerstad <kyrregjerstad@gmail.com>
2023-05-25 16:47:08 +0200

    add fullscreen img modal

--- src/components/modals/ImageModal.svelte
+++ src/components/modals/ImageModal.svelte
@@ -1,16 +1,40 @@
 <script lang="ts">
 	import type { Post } from "$lib/types";
 	import { PUBLIC_API_BASE_URL } from "$env/static/public";
+	import { blur } from "svelte/transition";
 
 	export let post: Post;
-
 	let src = `${PUBLIC_API_BASE_URL}${post.attributes.image.data.attributes.formats.large.url}`;
+
+	let showModal = false;
 </script>
 
-<div class="image-and-title-wrapper">
+<svelte:window
+	on:keydown={(e) => {
+		if (e.key === "Escape") {
+			showModal = false;
+		}
+	}}
+/>
+
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+<div class="image-and-title-wrapper" on:click={() => (showModal = true)}>
 	<h1>{post.attributes.title}</h1>
 	<img {src} alt={post.attributes.title} class="header-img" />
 </div>
+
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+{#if showModal}
+	<div
+		class="modal"
+		on:click={() => {
+			showModal = false;
+		}}
+		transition:blur
+	>
+		<img {src} alt={post.attributes.title} class="modal-img" />
+	</div>
+{/if}
 
 <style>
 	.image-and-title-wrapper {
@@ -36,5 +60,25 @@
 		display: inline;
 		position: relative;
 		z-index: 1;
+	}
+
+	.modal {
+		position: fixed;
+		top: 0;
+		left: 0;
+		width: 100vw;
+		height: 100vh;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		background-color: rgba(0, 0, 0, 0.8);
+		backdrop-filter: blur(5px);
+		z-index: 100;
+	}
+
+	.modal-img {
+		max-width: 80vw;
+		max-height: 80vh;
+		object-fit: cover;
 	}
 </style>
