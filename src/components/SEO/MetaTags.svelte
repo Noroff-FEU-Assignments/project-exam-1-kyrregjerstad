3803c84bd226a0515b54d273124ae3af724074d6
Kyrre Gjerstad <kyrregjerstad@gmail.com>
2023-05-27 23:42:28 +0200

    update seo tags

--- src/components/SEO/MetaTags.svelte
+++ src/components/SEO/MetaTags.svelte
@@ -8,4 +8,13 @@
 	<meta property="og:url" content={"https://www.blog.kyrre.dev"} />
 	<meta property="og:type" content={"article"} />
 	<meta property="og:description" content={config.metaDescription} />
+	<meta name="description" content={config.metaDescription} />
+	<link rel="canonical" href="https://www.blog.kyrre.dev" />
+	<meta property="og:title" content={config.title} />
+	<meta property="og:image" content={config.image} />
+	<meta name="twitter:card" content="summary_large_image" />
+	<meta name="twitter:site" content="@your-twitter-handle" />
+	<meta name="twitter:title" content={config.title} />
+	<meta name="twitter:description" content={config.metaDescription} />
+	<meta name="twitter:image" content={config.image} />
 </svelte:head>
